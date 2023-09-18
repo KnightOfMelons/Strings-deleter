b943e7ce769fb1e1dc8c3d99dfea340dc48068c4
KnightOfMelons <ilaprosmotrov@gmail.com>
2023-09-18 15:53:05 +0400

    Added another endcoding (just follow the instructions)

--- main.py
+++ main.py
@@ -13,25 +13,26 @@ filename_in_folders = input('\nВведите названия одного по
 first_line = int(input('\nС какой строки удалить (у меня было с 11)? '))
 end_line = int(input('\nДо какой строки удалить (до 29)? '))
 
-try:
-    # Проходимся по всем папкам внутри указанной папки
-    for dirpath, dirnames, filenames in os.walk(folder_path):
-        for filename in filenames:
-            # Если файл называется index.html
-            if filename == f'{filename_in_folders}':
-                # Составляем полный путь к файлу
-                file_path = os.path.join(dirpath, filename)
-                # Открываем файл на чтение и запись
-                with open(file_path, 'r', encoding='cp1251') as file_read:
-                    # Читаем все строки файла в список
-                    lines = file_read.readlines()
-                # Удаляем строки с 12 по 29
-                # del lines[11:29]
-                del lines[first_line:end_line]  # Но сейчас на выбор есть возможность.
-                # Открываем файл на запись
-                with open(file_path, 'w', encoding='cp1251') as file_write:
-                    # Записываем все оставшиеся строки в файл
-                    for line in lines:
-                        file_write.write(line)
-except Exception:
-    pass
+first_endcoding = input('\nКакую кодировку использовать для чтения файлов (изначально utf-8)? ')
+second_endcoding = input('\nКодировка для перезаписи файла (изначально utf-8)? ')
+# ИНОГДА МОЖЕТ БЫТЬ cp1251 ДЛЯ КИРИЛЛИЦЫ
+
+# Проходимся по всем папкам внутри указанной папки
+for dirpath, dirnames, filenames in os.walk(folder_path):
+    for filename in filenames:
+        # Если файл называется index.html
+        if filename == f'{filename_in_folders}':
+            # Составляем полный путь к файлу
+            file_path = os.path.join(dirpath, filename)
+            # Открываем файл на чтение и запись
+            with open(file_path, 'r', encoding=first_endcoding) as file_read:
+                # Читаем все строки файла в список
+                lines = file_read.readlines()
+            # Удаляем строки с 12 по 29
+            # del lines[11:29]
+            del lines[first_line:end_line]  # Но сейчас на выбор есть возможность.
+            # Открываем файл на запись
+            with open(file_path, 'w', encoding=second_endcoding) as file_write:
+                # Записываем все оставшиеся строки в файл
+                for line in lines:
+                    file_write.write(line)
